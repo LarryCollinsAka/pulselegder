20506e927e4c2024d33fa6c32172297b16c75e60
LarryCollinsAka <clatehlarry@gmail.com>
2026-01-19 14:44:52 +0100

    Initialize Api

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination: process.env.NODE_ENV === "development"
+          ? "http://127.0.0.1:8000/api/:path*" // Your local Python server
+          : "/api/",                           //# Vercel's automatic mapping
+      },
+    ];
+  },
 };
 
 export default nextConfig;
